16bedeee42d5741f7bbe752cd0caa1f815b8a2a7
Park Jae Yun <wodbs720@naver.com>
2023-03-08 06:39:16 +0900

    feat( home_page.dart ): 홈페이지 생성

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
