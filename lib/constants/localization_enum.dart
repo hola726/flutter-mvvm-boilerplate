58c30af6c0fd8ee6e234c0a28696524c78bdc1cc
Park Jae Yun <wodbs720@naver.com>
2023-03-13 06:48:55 +0900

    feat( localization_enum ): 요소 추가

--- lib/constants/localization_enum.dart
+++ lib/constants/localization_enum.dart
@@ -14,7 +14,11 @@ enum Localization {
   RECEIVE_TIMEOUT("수신 시간초과"),
   BAD_RESPONSE("응답 에러"),
   REMOTE_CANCEL("요청 취소"),
-  UNKNOWN("기타 오류");
+  UNKNOWN("기타 오류"),
+
+  RETRY_PLZ("다시시도 해주십시오"),
+  CONFIRM("확인"),
+  CANCEL("취소");
 
   const Localization(this.krDescription);
   final String krDescription;
